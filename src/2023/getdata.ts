00a2f8f555a86b90ffbb1038e4f1ec971a86ffc3
bananabread08 <rmkfermin@gmail.com>
2023-12-12 16:36:35 +0800

    Remove aoc inputs and update getData

--- src/2023/getdata.ts
+++ src/2023/getdata.ts
@@ -4,3 +4,7 @@ import fs from 'fs';
 export function getData(day: number, file: string) {
   return fs.readFileSync(path.join(__dirname, `/${day}/${file}`)).toString();
 }
+
+export function getFileData(file: string) {
+  return fs.readFileSync(path.join(process.cwd(), file)).toString();
+}
